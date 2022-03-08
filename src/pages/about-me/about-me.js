1a8936cf021f51802ceac5b308f935c5bd631767
Christopher Flodin <chris@Christophers-MacBook-Air.local>
2022-03-08 17:36:24 +0100

    Improved responsiveness

--- src/pages/about-me/about-me.js
+++ src/pages/about-me/about-me.js
@@ -1,11 +1,44 @@
 import "./about-me.scss";
 
 function AboutMe() {
+  const Interests = () => {
+    const intList = [
+      "Rock Climber",
+      "Videographer",
+      "Choir Singer",
+      "Exercise Enthusiast",
+      "D&D Nerd",
+      "Enjoys Outdoors",
+      "Agile Champion",
+      "Photographer",
+      "Coffee Lover",
+      "Stockholmer",
+      "Striving to Meditate Consistently",
+      "Web Developer",
+      "Product Manager",
+    ].map((interest, i, arr) => {
+      return (
+        <span key={interest} style={{ color: i % 2 == 0 ? "#fcd0a9" : "white" }} className="interest-text">
+          {interest} {i === arr.length - 1 ? "" : <span className="pipe">| </span>}
+        </span>
+      );
+    });
+    return <div className="interests">{intList}</div>;
+  };
+
   return (
     <section className="about-me">
-      <div className="profile"></div>
-      <div className="img-container">
+      <div className="profile-container">
         <img src={require("../../images/chris_profile.png")} alt="" />
+        <div className="text-container">
+          {<Interests />}
+          <div className="reach-out">
+            <div className="cta">Get in touch</div>
+            <div className="detail">+46 76 145 53 98</div>
+            <div className="detail">christopher.flodin@gmail.com</div>
+            <div className="detail">Stockholm, Sweden</div>
+          </div>
+        </div>
       </div>
     </section>
   );
